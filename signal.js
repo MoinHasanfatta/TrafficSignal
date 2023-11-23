062d083e5ad449e33aad5a728d5581e0f0058376
MoinHasanfatta <hasanfattamoin@gmail.com>
2023-11-23 16:17:40 +0530

    Signal Update

---
+++ signal.js
@@ -0,0 +1,19 @@
+let circles = document.querySelectorAll('.circle'),
+light = 0;
+
+setInterval( ()=>{
+    changeLight() ;
+}, 2000)
+
+function changeLight(){
+    circles[light].classList="circle";
+    light++;
+
+    if(light>2){
+        light=0;
+    }
+
+    const selectLight= circles[light];
+    selectLight.classList.add(selectLight.getAttribute("color"))
+
+}
